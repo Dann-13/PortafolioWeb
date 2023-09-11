a217a1d9ebcb3c2511d434b1e7283972fa5811f1
Dann-13 <dannyelgomezz.13@gmail.com>
2023-09-11 16:23:25 -0500

    Trancisiones

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,35 @@
+//framer motion
+import { motion } from 'framer-motion'
+//variants
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%'
+  },
+  animate: {
+    x: '0%',
+    width: '0%'
+  },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%']
+  }
+}
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen z-0
+      bg-[#2A303F]' variants={transitionVariants} initial='initial' animate='animate' exit='exit'
+        transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}>
+        
+      </motion.div>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen z-20
+      bg-[#20262E]' variants={transitionVariants} initial='initial' animate='animate' exit='exit'
+        transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut' }}>
+        
+      </motion.div>
+
     </>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css';
 //Components
 import Layout from '../components/Layout';
+import Transition from '../components/Transition'
+//router 
+import { useRouter } from 'next/router';
+//frameMOtion
+import { AnimatePresence, motion } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
-  return (  
-  <Layout>
-    <Component {...pageProps} /> 
-  </Layout>
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+
+      </AnimatePresence>
+
+    </Layout>
   )
 }
 
