005b6a800a3b0af2bbda76a64f052eafe177eead
Dann-13 <dannyelgomezz.13@gmail.com>
2023-09-14 16:05:52 -0500

    Cambios en sobre mi

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+import react, { useState } from "react";
 // icons
 import {
   FaHtml5,
@@ -14,12 +15,17 @@ import {
   SiAdobexd,
   SiAdobephotoshop,
 } from "react-icons/si";
-
+//components
 import Avatar from '../../components/Avatar'
+import Circles from '../../components/Circles'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
 
+import CountUp from "react-countup";
 //  data
 const aboutData = [
   {
+    id: 1,
     title: 'skills',
     info: [
       {
@@ -41,6 +47,7 @@ const aboutData = [
     ],
   },
   {
+    id: 2,
     title: 'awards',
     info: [
       {
@@ -54,6 +61,7 @@ const aboutData = [
     ],
   },
   {
+    id: 3,
     title: 'experience',
     info: [
       {
@@ -71,6 +79,7 @@ const aboutData = [
     ],
   },
   {
+    id: 4,
     title: 'credentials',
     info: [
       {
@@ -90,11 +99,100 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>
-    <div className="w-full h-full max-w-[737px] max-h-[678px] absolute">
-          <Avatar />
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="mb-[80px] py-28 bg-primary/30 text-center xl:text-left">
+      <Circles />
+      
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial='hidden'
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]">
+        <Avatar />
+      </motion.div>
+      <div className="container  lg:pt-10  mx-auto h-full flex flex-col items-center xl:flex-row gap-6" >
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="h2">
+            Si puedes <span className="text-accent">imaginarlo</span> puedes crearlo
+          </h2>
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+            Soy un amante de la tecnología desde que era niño, y mi curiosidad me ha llevado a
+            sumergirme profundamente en el mundo del desarrollo de software.
+            Durante mis años de estudio en la universidad, he adquirido conocimientos sólidos en
+            lenguajes de programación, diseño de software.
+          </p>
+          {/**Counters */}
+          <div>
+            <div>
+              {/**projets */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 
+  after:absolute after:top-0 after:right-0 flex flex-col items-center justify-center sm:justify-start">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Projectos
+                </div>
+              </div>
+
+            </div>
+          </div>
         </div>
-  </div>;
+        {/**Informacion */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h:[400px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {
+              aboutData.map((item, itemIndex) => {
+                return <div key={itemIndex}
+                  className={`${index === itemIndex &&
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                    }
+                  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
+                after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => {
+                    setIndex(itemIndex)
+                  }}
+                >
+                  {item.title}
+                </div>
+              })
+            }
+          </div>
+          <div className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4
+        items-center xl:items-start">
+            {
+              aboutData[index].info.map((item, itemIndex) => {
+                return (
+                  <div key={item.id} className="flex-1 flex flex-col md:flex-row
+                max-w-max gap-x-2  items-center text-white/60">
+                    {/**Title */}
+                    <div className="font-light items-center ">{item.title}</div>
+                    <div className="hidden md:flex">-</div>
+                    <div>{item.stage}</div>
+
+                    <div className="flex gap-x-4">
+                      {/**icons */}
+                      {item.icons?.map((icon, itemIndex) => {
+                        return <div className="text-2xl text-white">
+                          {icon}
+                        </div>
+                      })}
+                    </div>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+        </div>
+      </div>
+
+              
+    </div>
+  )
 };
 
 export default About;
