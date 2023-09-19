4d12905f1f3d8e8d2b488a11b5cf868d15c747ad
Dann-13 <dannyelgomezz.13@gmail.com>
2023-09-18 20:26:58 -0500

    Pagina projects y terminando estilos de servicios

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return <div className="absolute -left-36 -bottom-12  animate-pulse duration-75 z:10
+  w-[200px] xl:w-[260px]">
+    <Image 
+    src={'/bulb3.png'}
+    width={260}
+    height={200}
+    className="w-full h-full"
+    />
+  </div>;
 };
 
 export default Bulb;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,71 @@
+import { motion, } from 'framer-motion'
+import React, { useState } from 'react';
+import bulb2 from "../../public/bulb2.png"
+import bulb from "../../public/bulb.png"
 const Work = () => {
-  return <div>Work</div>;
+  const [expandedIndex, setEpandedIndex] = useState(null);
+  const handleCardClick = (index) => {
+    setEpandedIndex(index === expandedIndex ? -1 : index)
+  }
+  const cardVariants = {
+    expanded: {
+      width: 400
+    },
+    collapsed: {
+      width: 200
+    }
+  }
+  const cardImages = [bulb2, bulb]
+  const cardDescription = [
+    'esta es una descripcion de prueba para las card que hare sobre los proyects que tengo',
+    'esta es una descripcion de prueba para las card que hare sobre los proyects que tengo'
+
+  ]
+  return (
+    <div className='py-36'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center'>
+        <h1 className='text-3xl font-extrabold'>Projectos</h1>
+        <p className='mt-4 text-xl'> Revisa nuestros projectos</p>
+      </div>
+      <div className='mt-12 flex flex-col md:flex-row justify-center items-center gap-5'>
+        {[0, 1].map((index) => (
+          console.log("Ruta de la imagen:", cardImages[index].src),
+          <motion.div
+            key={index}
+            variants={cardVariants}
+            initial='collapsed'
+            animate={index === expandedIndex ? 'expanded' : 'collapsed'}
+            transition={{ duration: 0.5 }}
+            onClick={() => handleCardClick(index)}
+            style={{
+              backgroundImage: `url(${cardImages[index].src})`,
+            }}
+            className={`card cursor-pointer h-[500px] bg-cover bg-center rounded-[20px]
+          ${index === expandedIndex ? 'expanded' : ''} `}
+          >
+            <div className='card-content h-full flex flex-col justify-end'>
+              <div className='card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 
+              min-h-[100px] flex flex-col items-center justify-center'>
+                <h2 className='text-xl font-semibold text-white text-center'>
+                  Card {index + 1}
+                  </h2>
+                  {index === expandedIndex && (
+                    <p className='mt-2 text-gray-300 text-center'>
+                      {cardDescription[index]}
+                    </p>
+                  )
+
+                  }
+                </div>
+              
+            </div>
+
+          </motion.div>
+        ))}
+
+      </div>
+    </div>
+  )
 };
 
 export default Work;
